54c373218c4a95c603eb6ef557dc0e616c5923d8
césar <cesaralonso99@gmail.com>
2024-10-18 21:30:55 -0300

    feat: fonts and routes

---
+++ app/(tabs)/categorias.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Categorias = () => {
+  return (
+    <View>
+      <Text>Categorias</Text>
+    </View>
+  )
+}
+
+export default Categorias
